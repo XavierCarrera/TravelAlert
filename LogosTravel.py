5b63410cd698b5e34710cb94d137c8d825203d6a
Xavier Carrera <xavier.carrera1990@gmail.com>
2020-07-12 11:57:36 -0500

    Keyword system finished

--- LogosTravel.py
+++ LogosTravel.py
@@ -8,21 +8,34 @@ CIUDADES = {
 }
 
 def consulta_destino():
-    # print("Tendrás un bonito viaje")
+    print("Tendrás un bonito viaje")
     # consulta_ciudades = CIUDADES
     # consulta = []
-
+    
     # for resultado in consulta_ciudades:
 
-def admins_password(): 
+def admin_password(password):
+    if password == str("Sarajevo82"):
+        return True
+    else: 
+        return False
+
+def admin_user(user): 
+    if user == str("Xavi") or str("Dani") or str("Jann"): 
+        return True
+    else:
+        return False
+        print("Nombre de usuario o contraseña equivocada")
+    # while password == str("Sarajevo82"):
+    #         print("Bienvenido administrador! ")      
+    #         break
+
 
 def run():
 
-    mensaje = ''
-
     while(True):
 
-        opcion = str(input('''
+        action = str(input('''
         ••••••••••••••••••••••••••••••••••••••••••••••
                     S A F E // T R A V E L
         ••••••••••••••••••••••••••••••••••••••••••••••
@@ -38,21 +51,26 @@ def run():
 
         ''').upper())
 
-        if(opcion == "C"):
-            destino = int(input("¿Cual es tu próximo destino? "))
+        if(action == "C"):
+            destino = str(input("¿Cual es tu próximo destino? "))
             consulta = consulta_destino()
-            # print(consulta)
+            print(consulta)
 
-            if consulta_destino == True:
-                print("Consulta exitosa")
+            # if consulta_destino == True:
+            #     print("Consulta exitosa")
+            # else:
+            #     print("Lo sentimos. No tenemos información de tu destino")
+
+        elif(action == "E"):
+            user = input("Escribe tu nombre de usuario: ")
+            password = input("Hola {}, escribe tu contraseña: ".format(user))
+            # result_user = admin_user(user)
+            if admin_user(user) and admin_password(password) == True:
+                print("Bienvenido administrador! ")
             else:
-                print("Lo sentimos. No tenemos información de tu destino")
+                print("Nombre de usuario o contraseña equivocada")
 
-        elif(opcion == "E"):
-            print("Bienvenido administrador")
-            password = str(input("Escribe tu contraseña de acceso: "))
-
-        elif (opcion == "S"):
+        elif(action == "S"):
             quit()
         else:
             print("Elige una opción válida")
