06d0513f0131d4360c1a3ee0a3e2c5a0d82ef50d
Xavier Carrera <xavier.carrera1990@gmail.com>
2020-07-12 12:57:55 -0500

    Project Name Defined

---
+++ MiteraTravel.py
@@ -0,0 +1,83 @@
+
+CIUDADES = {
+        "Ciudad de México" : "Hay mucho tráfico de salida al aeropuerto",
+        "Buenos Aires" : "Hay una manifestación en la Plaza de Mayo",
+        "Bogotá" : "Se espera una lluvia muy fuerte",
+        "Lima" : "Alianza juega contra la U. No Acercarse al estadio",
+        "Santiago de Chile" : "Piñera se bebió un terremoto. Proceda con precaución",
+}
+
+def info_ciudades():
+    pass
+
+def consulta_destino():
+    lista_ciudades = []
+    lista_ciudades.append(CIUDADES)
+    # for ciudades in consulta_ciudades:
+            
+    print(lista_ciudades)
+    
+    # for resultado in consulta_ciudades:
+
+def admin_password(password):
+    if password == str("Sarajevo82"):
+        return True
+    else: 
+        return False
+
+def admin_user(user): 
+    if user == str("Xavi") or str("Dani") or str("Jann"): 
+        return True
+    else:
+        return False
+        print("Nombre de usuario o contraseña equivocada")
+    # while password == str("Sarajevo82"):
+    #         print("Bienvenido administrador! ")      
+    #         break
+
+
+def run():
+
+    while(True):
+
+        action = str(input('''
+        ••••••••••••••••••••••••••••••••••••••••••••••
+                    S A F E // T R A V E L
+        ••••••••••••••••••••••••••••••••••••••••••••••
+        
+        Bienvenida/o a SafeTravel.com. Aqui podras consultar sobre lo que está
+        sucediendo en tu destino de viaje. 
+
+        ¿Qué acción quieres llevar a cabo?
+
+                        [C]onsultar información de mi destino. 
+                        [E]ntrar a tu cuenta de administrador.
+                        [S]alir
+
+        ''').upper())
+
+        if action == "C":
+            destino = str(input("¿Cual es tu próximo destino? "))
+            consulta = consulta_destino()
+            print(consulta)
+
+            # if consulta_destino == True:
+            #     print("Consulta exitosa")
+            # else:
+            #     print("Lo sentimos. No tenemos información de tu destino")
+
+        elif action == "E":
+            user = input("Escribe tu nombre de usuario: ")
+            password = input("Hola {}, escribe tu contraseña: ".format(user))
+            if admin_user(user) and admin_password(password) == True:
+                print("Bienvenido administrador! ")
+            else:
+                print("Nombre de usuario o contraseña equivocada")
+
+        elif action == "S":
+            quit()
+        else:
+            print("Elige una opción válida")
+
+if __name__ == '__main__':
+    run()
